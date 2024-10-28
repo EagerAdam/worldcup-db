ee29a74e05e636fd710f36acbd645200368ccb3f
Adam Ratcliff <ar@ejaarent.com>
2024-10-28 14:09:43 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,17 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+cat games.csv | tail -n +2 | awk -F ',' '{print $3; print $4}' | sort | uniq | while read TEAM
+do
+  # Insert unique teams into teams table
+  $PSQL "INSERT INTO teams (name) VALUES ('$TEAM') ON CONFLICT (name) DO NOTHING;"
+done
+
+cat games.csv | tail -n +2 | while IFS=',' read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+
+  $PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);"
+done
